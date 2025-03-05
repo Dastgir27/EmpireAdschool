ef428ae63739e9c09903627b02cf418bfa3fa8c8
Dastgir <dastgir@millicent.in>
2025-03-05 19:26:40 +0530

    routes update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import Main from './views/Adschool/Main'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Main from './views/Adschool/Main';
 import './App.css';
+import ContactPage from './views/Adschool/ContactUs';
 
 function App() {
   return (
-    <div className="App">
-      <Main/>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
