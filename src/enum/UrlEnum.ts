e4b0eb5ea1a49557d6cf94e3445a9fc4c8b5ecb9
Dastgir27 <90137684+Dastgir27@users.noreply.github.com>
2025-03-05 18:29:41 +0530

    Delete src directory

--- src/enum/UrlEnum.ts
+++
@@ -1,4 +0,0 @@
-export enum UrlEnum {
-  CAMPAIGN_AUDIT_SERVICES_BASE_URL = "https://auditdataprovider-qa-usa.localiq-b2b-qanx1-us-west1.gke-hilton-als-svc.gannettdigital.com",
-  // CAMPAIGN_AUDIT_SERVICES_BASE_URL = "http://localhost:8080"
-}
